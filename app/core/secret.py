3a710b7cbb7884d8ee283a37222d8130e09ce6d8
denver-code <csigorek@gmail.com>
2022-12-21 23:37:27 +0000

    Added class with getters and all logic for Password

--- app/core/secret.py
+++ app/core/secret.py
@@ -10,3 +10,19 @@ async def generate_password(length=20):
 
 async def password_hash(password):
     return hashlib.sha256((password+settings.HASH_SALT).encode()).hexdigest()
+
+class Password():
+    def __init__(self):
+        self.password = ""
+    
+    def generate(self, length=20):
+        self.password = "".join(random.sample(string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation, length))
+    
+    @property
+    def hash(self):
+        return hashlib.sha256((self.password+settings.HASH_SALT).encode()).hexdigest()
+    
+    @property
+    def hex(self):
+        return self.password
+    
