763bb1a2b5207fe45358cc462604f7f18346599a
denver-code <csigorek@gmail.com>
2022-12-27 00:32:44 +0000

    Refactored upload fs logic

--- app/v1/private/foundation/fs.py
+++ app/v1/private/foundation/fs.py
@@ -7,84 +7,38 @@ from typing import Any, Dict
 
 fs_router = APIRouter(prefix="/fs")
 
-# TODO: Make single finction for downloading files
+async def upload_file(id: str, file: UploadFile, file_name: str, allowed_extensions: list):
+    if not await find_one("foundations_db", {"_id":id}):
+        return await raise_error_event(404, "Foundation not found.")
+
+    user_folder = f"templates/{id}/"
+    if not os.path.exists(user_folder):
+        os.makedirs(user_folder)
+    
+    file_extension = os.path.splitext(file.filename)[1]
+
+    # Check if the file extension is valid
+    if file_extension.lower() not in allowed_extensions:
+        return await raise_error_event(400, f"Invalid file format. Only {' and '.join(allowed_extensions)} are allowed.")
+
+    # Change the file name to "filename" + the file extension
+    file_name = file_name + file_extension
+
+    # Save the uploaded file to the user's folder
+    file.file.seek(0)
+    with open(f"{user_folder}/{file_name}", "wb") as f:
+        f.write(file.file.read())
+
+    return {"message": "file uploaded!"}
 
 @fs_router.post("/{id}/upload/template")
 async def upload_template_event(id: str, file: UploadFile = File(...), current_user: Dict[str, Any] = Depends(get_current_user)):
-    if not await find_one("foundations_db", {"_id":id}):
-        return await raise_error_event(404, "Foundation not found.")
-
-    user_folder = f"templates/{id}/"
-    if not os.path.exists(user_folder):
-        os.makedirs(user_folder)
-
-    # Get the file extension
-    file_extension = os.path.splitext(file.filename)[1]
-
-    # Check if the file extension is valid
-    if file_extension.lower() not in [".png", ".jpg"]:
-        return await raise_error_event(400, "Invalid file format. Only PNG and JPG are allowed.")
-
-    # Change the file name to "template" + the file extension
-    file_name = "template" + file_extension
-
-    # Save the uploaded file to the user's folder
-    file.file.seek(0)
-    with open(f"{user_folder}/{file_name}", "wb") as f:
-        f.write(file.file.read())
-
-    return {"message": "file uploaded!"}
-
+    return await upload_file(id, file, "template", [".png"])
 
 @fs_router.post("/{id}/upload/default_avatar")
 async def upload_default_avatar_event(id: str, file: UploadFile = File(...), current_user: Dict[str, Any] = Depends(get_current_user)):
-    if not await find_one("foundations_db", {"_id":id}):
-        return await raise_error_event(404, "Foundation not found.")
-
-    user_folder = f"templates/{id}/"
-    if not os.path.exists(user_folder):
-        os.makedirs(user_folder)
-
-    # Get the file extension
-    file_extension = os.path.splitext(file.filename)[1]
-
-    # Check if the file extension is valid
-    if file_extension.lower() not in [".png", ".jpg"]:
-        return await raise_error_event(400, "Invalid file format. Only PNG and JPG are allowed.")
-
-    # Change the file name to "template" + the file extension
-    file_name = "avatar" + file_extension
-
-    # Save the uploaded file to the user's folder
-    file.file.seek(0)
-    with open(f"{user_folder}/{file_name}", "wb") as f:
-        f.write(file.file.read())
-
-    return {"message": "file uploaded!"}
-
+    return await upload_file(id, file, "avatar", [".png"])
 
 @fs_router.post("/{id}/upload/font")
-async def upload_default_avatar_event(id: str, file: UploadFile = File(...), current_user: Dict[str, Any] = Depends(get_current_user)):
-    if not await find_one("foundations_db", {"_id":id}):
-        return await raise_error_event(404, "Foundation not found.")
-
-    user_folder = f"templates/{id}/"
-    if not os.path.exists(user_folder):
-        os.makedirs(user_folder)
-
-    # Get the file extension
-    file_extension = os.path.splitext(file.filename)[1]
-
-    # Check if the file extension is valid
-    if file_extension.lower() != "ttf":
-        return await raise_error_event(400, "Invalid file format. Only ttf are allowed.")
-
-    # Change the file name to "template" + the file extension
-    file_name = "font" + file_extension
-
-    # Save the uploaded file to the user's folder
-    file.file.seek(0)
-    with open(f"{user_folder}/{file_name}", "wb") as f:
-        f.write(file.file.read())
-
-    return {"message": "file uploaded!"}
+async def upload_font_event(id: str, file: UploadFile = File(...), current_user: Dict[str, Any] = Depends(get_current_user)):
+    return await upload_file(id, file, "font", [".ttf"])
