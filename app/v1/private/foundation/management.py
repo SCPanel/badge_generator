3f2b34cad8542f3729f753d6f23848dd5d07808a
denver-code <csigorek@gmail.com>
2022-12-31 16:22:41 +0000

    Added foudnation check for management

--- app/v1/private/foundation/management.py
+++ app/v1/private/foundation/management.py
@@ -1,13 +1,14 @@
 from shutil import rmtree
 from fastapi import APIRouter
 from app.core.database import find_one, delete_one, delete
+from app.core.error import raise_error_event
 
 management_router = APIRouter(prefix="/management")
 
 @management_router.delete("/{id}/delete")
 async def delete_foundation_event(id: str):
     if not await find_one("foundations_db", {"_id": id}):
-        return
+        return await raise_error_event(404, "Foundation not found.")
     
     await delete_one("foundations_db", {"_id": id})
     await delete("users_db", {"alias": id})
