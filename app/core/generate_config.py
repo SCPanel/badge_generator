a2cfe504db9bd3b06b217442501a8bf733f46944
denver-code <csigorek@gmail.com>
2022-12-31 16:21:38 +0000

    Ready generate_config feature

--- app/core/generate_config.py
+++ app/core/generate_config.py
@@ -1,4 +1,5 @@
 import json
+from os import mkdir, path
 
 def generate_config(
         font_size: int,
@@ -9,11 +10,21 @@ def generate_config(
     base_config = open("templates/_template.json", "r")
     base_config = json.load(base_config)
 
-    base_config["root_id"] = ""
+    base_config["root_id"] = foundation["_id"]
+    base_config["owner"] = foundation["owner"]
     base_config["font"]["size"] = font_size
     base_config["avatar_position"] = avatar_position
     base_config["components"] += components
 
-    print(base_config)
+    if optional:
+        base_config["optional"] += optional
 
+    
+    if not path.exists(f"templates/{foundation['_id']}"):
+        mkdir(f"templates/{foundation['_id']}")
+
+    with open(f"templates/{foundation['_id']}/template_schema.json", "w") as config_file:
+        json.dump(base_config, config_file, indent=4)
+
+    return base_config
     
--- app/v1/private/foundation/schema.py
+++ app/v1/private/foundation/schema.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter
+from app.core.database import find_one
+from app.core.error import raise_error_event
 from app.core.generate_config import generate_config
 from app.core.schemas.foundation_schema import FoundationSchema
 
 schema_router = APIRouter(prefix="/schema")
 
-@schema_router.post("/new")
-async def new_schema(foundation_schema: FoundationSchema):
+@schema_router.post("/{id}/new")
+async def new_schema(foundation_schema: FoundationSchema, id: str):
+    _foundation = await find_one("foundations_db", {"_id": id})
+    if not _foundation:
+        return await raise_error_event(404, "Foundation not found.")
+
     _foundation_schema = foundation_schema.dict()
 
-    generate_config(
+    config = generate_config(
         **_foundation_schema,
-        foundation=0
+        foundation=_foundation
     )
+
+    if not config:
+        return await raise_error_event(500, "Something went wrong, try again later.")
+    
+    return config
