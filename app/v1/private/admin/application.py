3e6713a5af50748b88e0c4ee581b8302ebd91a18
Denver <csigorek@gmail.com>
2023-01-04 20:05:21 +0000

    Some application model fixes

--- app/v1/private/admin/application.py
+++ app/v1/private/admin/application.py
@@ -38,12 +38,18 @@ async def approve_application_event(id: str):
         "is_default_password": True
     }
 
-    foundation["project_details"] = {
+    foundation["details"] = {
+        "name": foundation["project_name"],
+        "owner_name": foundation["name"],
+        "description": foundation["description"],
         "secret_key": _secret_key.hash,
         "password": _temp_password.hash
     }
     foundation["expire"] = 1
-    del foundation["processed"]
+
+    to_del = ["name", "project_name", "description", "processed"]
+    for item in to_del:
+        del foundation[item]
 
     _user_temp_password = Password() # dont forget send that shit via email
     _user_temp_password.generate()
@@ -57,7 +63,7 @@ async def approve_application_event(id: str):
         "password": _user_temp_password.hash,
         "secret_key": _user_secret_key.hash,
         "alias": foundation["_id"],
-        "interrnal_alias": ""
+        "internal_alias": "scp_badge_system_user"
     }
     foundation["owner"] = user["_id"]
 
@@ -69,13 +75,13 @@ async def approve_application_event(id: str):
         email=foundation["email"],
         subject="Foundation confirmation!",
         content=f"""
-        Hi {foundation["name"]}! <br>
-        We are glad to let you know that your foundation named "{foundation["project_name"]}" has been confirmed by our admins.<br>
+        Hi {foundation["details"]["owner_name"]}! <br>
+        We are glad to let you know that your foundation named "{foundation["details"]["name"]}" has been confirmed by our admins.<br>
         To continue using our platform, please follow instructions below:<br>
         1. Go to our website and enter signin form<br>
-        2. Use your email "{foundation["email"]}" and default password(you have to change he after login) " {_user_temp_password.hex} "<br>
+        2. Use your email "{foundation["email"]}" and default password(you have to change he after login) " {_user_temp_password.hex} " <br>
         3. Enter this SECRET KEY(you will get a list of other keys which will be used while using platform) " {_user_secret_key.hex} " <br>
-        4. Follow instructions and add your "{foundation["project_name"]}" Foundation details. <br>
+        4. Follow instructions and add your "{foundation["details"]["name"]}" Foundation details. <br>
 
         Password of your Foundation: " {_temp_password.hex} " <br>
         Secret key of your Foundation: " {_secret_key.hex} "
